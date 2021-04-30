a69f284824d890ad2df082ec1c60e7472d1c92bf
jaspe <Jasper_versantvoort@hotmail.com>
2021-04-30 11:40:05 +0200

    Labels en title fix

--- Afvink_2.py
+++ Afvink_2.py
@@ -50,13 +50,10 @@ def matplotlib2(hoogtes1, hoogtes2, labels, naam1, naam2):
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Counts')
-    ax.set_title('Counts van '+naam1+" "+naam2)
+    ax.set_title('Counts van '+naam1+" and "+naam2)
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
     ax.legend()
-
-    # ax.bar_label(rects1, padding=3)
-    # ax.bar_label(rects2, padding=3)
 
     fig.tight_layout()
 
